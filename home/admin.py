3c7ac991ba02cd24843d425003936589fd388c60
Duong Minh Chien <minhchienwikipedia@gmail.com>
2022-11-14 14:08:47 +0700

    Add foreign keys, django-extensions

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, ItemType
 
 # Register your models here.
 
 
+class ItemTypeInline(admin.StackedInline):
+    model = Item
+    extra = 0
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'status')
     search_fields = ['title']
+    raw_id_fields = ['user']
+    
 
+class ItemTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ['name']
+    inlines = [ItemTypeInline]
 
 admin.site.register(Item, ItemAdmin)
+admin.site.register(ItemType, ItemTypeAdmin)
--- home/models.py
+++ home/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL
+
+
+class ItemType(models.Model):
+    name = models.TextField()
+
 class Item(models.Model):
+    user = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=255)
     status = models.BooleanField(default=False)
     image_url = models.TextField()
+    type = models.ForeignKey(ItemType, blank=True, null=True, on_delete=models.SET_NULL)
 
